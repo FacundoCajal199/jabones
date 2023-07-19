6026abafc3b189ad48fc50dbe87db70de1f99759
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-07-19 00:22:02 -0300

    probar nuevamente

--- src/components/Titular.jsx
+++ src/components/Titular.jsx
@@ -3,7 +3,7 @@ const Titular = () => {
   return (
     <>
       <h1 className="text-center py-5">
-        Descubre el negocio que salome y mas de{" "}
+        Descubre el negocio que salome y mas de
         <b className="text-info">
           2533 mujeres han usado para cambiar su vida y la de su familia en
           menos de 90 dias
